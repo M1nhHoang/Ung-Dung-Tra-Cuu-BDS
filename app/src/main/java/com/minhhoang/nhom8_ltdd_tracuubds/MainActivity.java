d39b13c13ccbda273f1851b3ddd710c6b597e138
M1nhHoang <boydammedamsamcotdien@gmail.com>
2023-10-23 20:25:17 +0700

    first commit

---
+++ app/src/main/java/com/minhhoang/nhom8_ltdd_tracuubds/MainActivity.java
@@ -0,0 +1,15 @@
+ 
+package com.minhhoang.nhom8_ltdd_tracuubds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
