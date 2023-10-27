43d6972aa92c95e94a2433521bc4b8829a799309
L3Cu0ng <lequangbaocuong@gmail.com>
2023-10-28 00:29:31 +0700

    Sửa lại layout hoso_hotro

---
+++ app/src/main/java/com/minhhoang/nhom8_ltdd_tracuubds/Hotro_main.java
@@ -0,0 +1,14 @@
+package com.minhhoang.nhom8_ltdd_tracuubds;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Hotro_main extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hoso_hotro);
+    }
+}
+
+
