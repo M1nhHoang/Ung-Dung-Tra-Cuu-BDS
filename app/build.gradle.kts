9a2b68cd66ddd957a0b1287aeb776edfb93d8b49
M1nhHoang <boydammedamsamcotdien@gmail.com>
2023-10-27 16:51:36 +0700

    fix UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.minhhoang.nhom8_ltdd_tracuubds"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.minhhoang.nhom8_ltdd_tracuubds"
         minSdk = 31
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
